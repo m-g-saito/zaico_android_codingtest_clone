d7eb577012bcba7b887978ca0293435e546ccf57
m-g-saito <azoyg72515261@gmail.com>
2025-05-05 21:26:25 +0900

    MainActivityを修正

--- app/src/main/java/jp/co/zaico/codingtest/MainActivity.kt
+++ app/src/main/java/jp/co/zaico/codingtest/MainActivity.kt
@@ -2,38 +2,52 @@ package jp.co.zaico.codingtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import jp.co.zaico.codingtest.databinding.ActivityMainBinding
+import jp.co.zaico.codingtest.ui.createinventory.CreateInventoryActivity
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupViewBinding()
+        setupNavigation()
+        setupFab()
+    }
 
+    private fun setupViewBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_content_main) as? NavHostFragment ?: return
+        navController = navHostFragment.findNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
 
-        binding.fab.setOnClickListener { view ->
-            startActivity(AddActivity.createIntent(this))
+    private fun setupFab() {
+        binding.fab.setOnClickListener {
+            val intent = CreateInventoryActivity.createIntent(this)
+            startActivity(intent)
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
 }
